94f46856821b9fabf63aba2d201dce45d600cad7
Esmat Farahi <esmatullah.farahi@gmail.com>
2023-08-16 21:27:06 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+        const apiKey = '';
+        const weatherContainer = document.getElementById('weather-info');
+        const searchButton = document.getElementById('search-button');
+        const cityInput = document.getElementById('city-input');
+
+       
