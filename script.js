98ee617d492696483391211c5838acd71e62b9f5
Esmat Farahi <esmatullah.farahi@gmail.com>
2023-08-17 10:40:38 -0400

    create renderWeatherData function

--- script.js
+++ script.js
@@ -11,16 +11,40 @@
 
       async function fetchWeatherData(city) {
         try{
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+              const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
               const response = await fetch(url);
               const data = await response.json();
-              console.log(data);
+            //   console.log(data);
+              renderWeatherData(data);
+
         }
              catch (error) {
                 weatherContainer.innerHTML = 'Error fetching weather data';
             }
         }
 
+        function renderWeatherData(data) {
+
+            const weatherDetails = data.weather[0].description;
+            const temperature = (data.main.temp - 273.15).toFixed(2); // Convert Kelvin to Celsius
+            const feelsLike = (data.main.feels_like - 273.15).toFixed(2); // Convert Kelvin to Celsius
+
+            const weatherInfo = `
+                <h2><b>Today weather in ${data.name} </b></h2>
+                <p><b>Weather:<b> ${weatherDetails}</p>
+                <p><b>Temperature:<b> ${temperature}°C</p>
+                <p><b>Feels Like:<b> ${feelsLike}°C</p>
+                <p><b>Humidity:</b> ${data.main.humidity} %</p>
+                <p><b>Pressure:</b> ${data.main.pressure} hPa</p>
+                <p><b>Wind Speed:</b> ${data.wind.speed} m/s</p>
+            `;
+
+            weatherContainer.innerHTML = weatherInfo;
+
+            }
+        
+
+        
             
     
    
